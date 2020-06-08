c6173413346f44c5fa7a865f4ac3d8adf7b7c46f
lessuiz <leonardoparreira2018@gmail.com>
2020-06-08 11:05:17 -0300

    Added comments

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,8 +1,12 @@
 def caesar_cipher(string, shift)
+  # Alphabet letters split into an array
   alphabet = "abcdefghijklmnopqrstuvwxyz".split("")
+  # String passed into the method is split into an array
   array_of_letters = string.split("")
+  # Create an empty array that will hold the new letters
   new_letters = []
   for letter in array_of_letters do
+    # If the letter variable is not an actual letter it will just be pushed into the array
     unless ('a'..'z').include?(letter) || ('A'..'Z').include?(letter)
       new_letters.push(letter)
       next
@@ -10,12 +14,15 @@ def caesar_cipher(string, shift)
     downcase_letter = letter.downcase
     index = alphabet.find_index(downcase_letter)
     new_index = index - shift
+    # If the new index is not bigger than 0 it will be added to 26 until it is
     until new_index > 0 do
       new_index += 26
     end
+    # Same if it is bigger than 26, but in this case it will be subtracted
     until new_index < 26 do
       new_index -= 26
     end
+    # Push the letter with the correct case to the letters array
     if ('a'..'z').include?(letter)
       new_letters.push(alphabet[new_index])
       next
@@ -24,7 +31,9 @@ def caesar_cipher(string, shift)
       next
     end
   end
+  # Join the letters into an array
   new_phrase = new_letters.join("")
+  # Return the phrase
   new_phrase
 end
 
